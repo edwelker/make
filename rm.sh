789a5278cf0112922a08ebbb3cdffbccaeb3d31e
Eddie Welker <eddie.welker@gmail.com>
2014-03-30 23:06:18 -0400

    whatever

--- rm.sh
+++ rm.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
 
 #find all of the *.* type files in the current directory and remove them
+#including this file :)
 find . -maxdepth 1 -name "*.*" | xargs rm
+
+#prune the .git directory results, or print the name
+#find . -name .git -prune -o -name "*.*"
